f59d940adc532937f7e789b6ebce4730b8934e88
BT_2S14_22 <email@>
2024-09-17 10:59:30 -0400

    Math edits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// ISSUES: Rounding problems (will round down), math error in total bill calculation
+
 public class Main {
     public static void main(String[] args) {
         // Starting statements
@@ -8,6 +10,7 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("_______________________");
         System.out.println("Tip Calculator");
+        System.out.println();
 
         // Original bill
         System.out.print("Enter your billing amount: ");
@@ -27,13 +30,13 @@ public class Main {
         // Math
         double tipMultiplier = (double) tipPercent / 100;       // Percent conversion
         double tipTotal = tipMultiplier * bill;                 // The total tip
-        tipTotal = (int)((tipTotal + 0.5)*100)/100.0;
-        double totalBill = tipTotal + bill;                     // The total bill for everyone WITH tip
-        totalBill = (int)((totalBill+0.5)*100)/100.0;
+        tipTotal = (int)(tipTotal*100)/100.0;
+        double totalBill = (tipMultiplier + 1) * bill;                     // The total bill for everyone WITH tip
+        totalBill = (int)(totalBill*100)/100.0;
         double tipPer = tipTotal / numPpl;                      // The tip per person
-        tipPer = (int)((tipPer+0.5)*100)/100.0;
+        tipPer = (int)(tipPer*100)/100.0;
         double totalPer = totalBill / numPpl;                   // The total bill and tax for each person
-        totalPer = (int)((totalPer+0.5)*100)/100.0;
+        totalPer = (int)(totalPer*100)/100.0;
 
         //Print statements
         System.out.println("");
